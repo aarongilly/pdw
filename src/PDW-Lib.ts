bbcad850add0002f53d1186626bd7a229cbed183
Aaron Gillespie <aarongilly@gmail.com>
2021-07-25 00:14:28 -0500

    began translation of documentation built a lot of interfaces. Realized StorageConnector is going to be hugely important.

--- src/PDW-Lib.ts
+++ src/PDW-Lib.ts
@@ -55,12 +55,173 @@ export default class PDW {
   }
 }
 
+/*
 interface testInterface {
   propOne: string;
   propTwo: number;
   optProp?: string;
   funProp(): string;
   //   funOne:
+}
+*/
+
+/**
+ * Minimum viable manifest. Just a label.
+ *
+ * All other manifest properties have default
+ * valuse that can be assumed or are calculated.
+ */
+interface MinimumManifest {
+  /**
+   * Human-readable name. Like a column header in Excel.
+   */
+  _label: string;
+}
+
+/**
+ * Full manifest including all properties
+ */
+interface FullManifest extends MinimumManifest {
+  /**
+   * The manifest identifier
+   */
+  _mid: string;
+  /**
+   * An emoji. Shorthand for the manifest.
+   */
+  _emoji: string;
+  /**
+   * What length of time an entry covers
+   */
+  _scope: Scope;
+  /**
+   * Earliest-dated entry's entry.periodEnd
+   */
+
+  _first: Date;
+  /**
+   * Latest-dated entry's entry.periodEnd
+   */
+  _latest: Date;
+  /**
+   * Still being recorded.
+   */
+  _active: boolean;
+  /**
+   * Tags for grouping alike manifests.
+   */
+  _tags: ManifestTag[];
+  /**
+   * Array of objects describing points.
+   */
+  _points: PointsManifest[];
+  /**
+   * A longer-form description.
+   */
+  _desc: string;
+}
+
+/**
+ * A simple tag to group alike Manifests
+ *
+ * Surrogate key "_tid" (Tag ID) is used to
+ * support changing tag labels without having
+ * to actually change the _tid associated with
+ * every Manifest with that tag.
+ */
+interface ManifestTag {
+  _tid: string; //surrogate key
+  _label: string; //human-readable tag
+}
+
+/**
+ * Minimum viable entry. These two criteria
+ * must be included for any valid entry. Also
+ * there should not be any two entries that
+ * share the same _mid and _period.
+ */
+interface MinimumEntry {
+  _mid: string;
+  _period: string;
+}
+
+/**
+ * Full entries include all required properties.
+ */
+interface FullEntry extends MinimumEntry {
+  _eid: string;
+  _created: Date;
+  _updated: Date;
+  _source: string;
+  _note: string;
+  _points?: EntryPointMap;
+  _periodEnd: Date;
+  _deleted: boolean;
+}
+
+/**
+ * Entry Points are a map keyed with an
+ * arbitrary quantity of _pid identifiers
+ */
+interface EntryPointMap {
+  1?: any;
+  2?: any;
+}
+
+/**
+ * To support portability & interoperability,
+ * any storage provider can be used that meets
+ * this interface. This is the whole point, really.
+ */
+interface StorageConnector {
+  /**
+   * This would be the main "data getter". I need
+   * to think about this more.
+   * @param params an object to filter against
+   */
+  getEntries(params: QueryParams): FullEntry[] | FullEntry;
+  /**
+   * The main "data setter" for entries.
+   * @param entries an array of entries to bulk write
+   * @param forceUpdate if entry.updated should be set
+   *     *regardless* of if any data changed. Defaults false.
+   */
+  setEntries(entries: FullEntry[], forceUpdate?: boolean): any;
+  /**
+   * Return all Manifests' data.
+   */
+  getManifests(): FullManifest[];
+  /**
+   * The main "data setter" for manifests.
+   * @param manifests Manifest data to create or update
+   */
+  setManifests(manifests: FullManifest[] | FullManifest): any;
+}
+
+/**
+ * These are the ways to search for and shape entry data.
+ */
+interface QueryParams {
+  from?: ISOString | Date;
+  to?: ISOString | Date;
+  createdFrom?: ISOString | Date;
+  createdTo?: ISOString | Date;
+  updatedFrom?: ISOString | Date;
+  updatedTo?: ISOString | Date;
+  eid?: string | string[];
+  mid?: string | string[];
+  tid?: string | string[];
+  tag?: string | string[];
+  includeDeleted?: boolean;
+  onlyDeleted?: boolean;
+  includeInactive?: boolean;
+  onlyInactive?: boolean;
+  scope?: Scope | Scope[];
+  terse?: any;
+  verbose?: any;
+  shape?: Shape;
+  includeEmptyPeriods?: boolean;
+  rollupMethod?: RollupMethod;
 }
 
 class Manifest {
@@ -123,6 +284,14 @@ class Period {
   //@TODO
 }
 
+//@TODO - think about this
+enum Shape {
+  "a",
+  "b",
+  "c",
+  "d"
+}
+
 /**
  * The valid scopes a Manifest can take.
  */
@@ -131,18 +300,28 @@ enum Scope {
   "day",
   "week",
   "month",
+  "quarter",
   "year"
 }
 
-interface IRollupMethod {
-  label: string;
-  rollup(): any;
-}
+// interface RollupMethod {
+//   label: string;
+//   rollup(): any;
+// }
 
-enum PointFormat {}
+enum PointFormat {
+  "@",
+  "#",
+  "#.#",
+  "yes/no",
+  "(hide)",
+  "list",
+  "json"
+}
 
 enum PointType {}
 
+//@TODO - is this really an enum?
 enum RollupMethod {}
 
 /**
