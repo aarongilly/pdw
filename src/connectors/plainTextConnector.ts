d79d3f3b1c9607c2a4a21aee78f2f824b92d1c0b
Aaron Gillespie <aarongilly@gmail.com>
2023-02-18 15:58:04 -0600

    some txt file stuff for ref

--- src/connectors/plainTextConnector.ts
+++ src/connectors/plainTextConnector.ts
@@ -26,3 +26,24 @@ function testCreate(){
 }
 
 testCreate();
+
+/*
+
+//How do we tie into the librarys in development?
+    
+    // writeFile function with filename, content and callback function
+    fs.writeFile('fs-test/newfile.txt', 'Learn Node FS module', function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+
+    // read file sample.html
+    fs.readFile('fs-test/newfile.txt',
+    // callback function that is called when reading file is done
+    function(err, data) {
+        if (err) throw err;
+        // data is a buffer containing file content
+        console.log(data.toString('utf8'))
+    });
+
+*/
