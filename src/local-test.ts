26197f6d78ba6499be305d41635d0aadf932a821
Aaron Gillespie <aarongilly@gmail.com>
2023-04-15 11:40:04 -0500

    built for testing implicit file merges

--- src/local-test.ts
+++ src/local-test.ts
@@ -3,19 +3,25 @@ import { FileConnector } from "./connectors/fileConnector.js";
 // import { sampleDefinitions } from './sampleData.js';
 
 const pdw = PDW.getInstance();
-const filename = 'fs-test/ExcelDevFile2.xlsx';
 const fileConnector = new FileConnector();
-
 pdw.registerConnection(fileConnector);
 
-(<FileConnector> pdw.connection).loadFromExcel(filename);
+//Testing implicit merge
+loadFile('fs-test/PDW-OutFile.xlsx')
+loadFile('fs-test/PDW-OutFile2.xlsx')
 
-const localDefCopy = pdw.getDefs(['Event', 'doae', 'seae']);
 
-console.log(localDefCopy);
+// const localDefCopy = pdw.getDefs(['Event', 'doae', 'seae']);
+
+// console.log(localDefCopy);
 
 // pdw.setDefs(sampleDefinitions);
-// (<FileConnector> pdw.connection).writeToFile('excel', filename);
+
+let outFilename = 'fs-test/PDW-OutFile3.xlsx';
+(<FileConnector> pdw.connection).writeToFile('excel', outFilename);
 
 // console.log(makeUID());
 
+function loadFile(fileName: string){
+    (<FileConnector> pdw.connection).loadFromExcel(fileName);
+}
